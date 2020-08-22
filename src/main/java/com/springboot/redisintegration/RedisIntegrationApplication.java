7a376c55b7848aada65d88babd380cae00e5a85c
Akash Solanki <akash.solankey@gmail.com>
2020-08-22 20:16:00 +0530

    Springboot initialized

---
+++ src/main/java/com/springboot/redisintegration/RedisIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.redisintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/redisintegration/RedisIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.redisintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
