288bfbaa0fca4d707480a2377d7134db6c72fa21
Akash Solanki <akash.solankey@gmail.com>
2020-09-29 15:39:23 +0530

    redis cluster bitnami

--- src/main/java/com/springboot/redisintegration/RedisConfiguration.java
+++ src/main/java/com/springboot/redisintegration/RedisConfiguration.java
@@ -1,13 +1,17 @@
 package com.springboot.redisintegration;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.inject.Inject;
 
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisSentinelConfiguration;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
+import org.springframework.data.redis.connection.RedisNode;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
@@ -17,25 +21,35 @@ public class RedisConfiguration {
 	@Inject
     private RedisProperties redisProperties; 
 	
-	@Lazy
 	@Bean
-	protected RedisConnectionFactory jedisConnectionFactory() {
-		RedisSentinelConfiguration sentinelConfig = new RedisSentinelConfiguration()
-				.master(redisProperties.getSentinel().getMaster());
-		redisProperties.getSentinel().getNodes().forEach(s -> {
-			sentinelConfig.sentinel(s, redisProperties.getPort());
-		}); 
-		// sentinelConfig.setPassword(RedisPassword.of(redisProperties.getPassword()));
-		RedisConnectionFactory factory = new JedisConnectionFactory(sentinelConfig);
-		return factory;
+	public JedisConnectionFactory getConFactory() {
+		String clusterNodes = "localhost:6370,localhost:6371,localhost:6372,localhost:6373,localhost:6374,localhost:6375";
+        List<RedisNode> redisNodes = Arrays.stream(clusterNodes
+                .split(","))
+                .map(s -> {
+                    String[] singleNode = s.split(":");
+                    return new RedisNode(singleNode[0], Integer.parseInt(singleNode[1]));
+                }).collect(Collectors.toList());
+
+        RedisClusterConfiguration redisClusterConfiguration = new RedisClusterConfiguration();
+        redisClusterConfiguration.setPassword("pass");
+        redisClusterConfiguration.setClusterNodes(redisNodes);
+        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(redisClusterConfiguration);
+        jedisConnectionFactory.afterPropertiesSet();
+        return jedisConnectionFactory;
 	}
 	 
 	
 	@Lazy
 	@Bean
 	public RedisTemplate<String, Object> redisTemplate() {
-	    RedisTemplate<String, Object> template = new RedisTemplate<>();
-	    template.setConnectionFactory(jedisConnectionFactory());
+	    RedisTemplate<String, Object> template= null;
+	    try {
+	    	template = new RedisTemplate<>();
+		    template.setConnectionFactory(getConFactory());
+	    } catch(Exception e) {
+	    	e.printStackTrace();
+	    }
 	    return template;
 	}
 }
--- src/main/java/com/springboot/redisintegration/RedisIntegrationApplication.java
+++ src/main/java/com/springboot/redisintegration/RedisIntegrationApplication.java
@@ -1,18 +1,7 @@
 package com.springboot.redisintegration;
 
-import javax.inject.Inject;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisSentinelConfiguration;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootApplication
 public class RedisIntegrationApplication {
